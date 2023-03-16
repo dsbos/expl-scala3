d50dc032aa6f7f8807d3261f49a1a217151c1d54
Daniel Barclay <daniel.barclay@qomplx.com>
2023-03-15 22:10:46 -0400

    TopLevelMethodOverloading:  Added.

---
+++ src/main/scala/com/us/dsb/scala3/misc/TopLevelMethodOverloading.scala
@@ -0,0 +1,6 @@
+package com.us.dsb.scala3.misc {
+  //def overloadedMethod(x: Long): Unit = () // "... overloadedMethod is already defined as method overloadedMethod in ..."
+}
+package com.us.dsb.scala3.misc {
+  def overloadedMethod(x: Int): Unit = ()
+}
