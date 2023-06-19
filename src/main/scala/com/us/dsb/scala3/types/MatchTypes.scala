f5997cadfbf6182f98bb82673df81e8deeb55325
Daniel Barclay <daniel.barclay@qomplx.com>
2023-06-18 21:31:47 -0400

    Added (empty) MatchTypes.

---
+++ src/main/scala/com/us/dsb/scala3/types/MatchTypes.scala
@@ -0,0 +1,9 @@
+package com.us.dsb.scala3.types
+
+object MatchTypes {
+
+  // https://dotty.epfl.ch/docs/reference/new-types/match-types.html
+  // https://www.youtube.com/watch?v=YiPQxIQZn5M
+
+
+}
